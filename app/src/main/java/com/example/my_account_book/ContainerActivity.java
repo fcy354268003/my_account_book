a9d1323e7cef3c226ab538bff33fb34cf608d0e2
fcy <1575047842@qq.com>
2020-07-09 18:55:26 +0800

    git 测试

--- app/src/main/java/com/example/my_account_book/ContainerActivity.java
+++ app/src/main/java/com/example/my_account_book/ContainerActivity.java
@@ -15,7 +15,7 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class ContainerActivity extends AppCompatActivity {
     private BottomNavigationView bottomNavigationView;
-    private Fragment[] fragment = new Fragment[]{new summaryFragment(), new TodayFragment(), new mineFragment(),};
+    private Fragment[] fragment = new Fragment[]{new summaryFragment(), new TodayFragment()};
     private Fragment currentFragment;
     public static String time;
     @Override
@@ -30,23 +30,18 @@ public class ContainerActivity extends AppCompatActivity {
 
     private void initController() {
         bottomNavigationView = findViewById(R.id.bottom_menu);
-        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-                switch (menuItem.getItemId()) {
-                    case R.id.summary:
-                        currentFragment = fragment[0];
-                        break;
-                    case R.id.today:
-                        currentFragment = fragment[1];
-                        break;
-                    case R.id.mine:
-                        currentFragment = fragment[2];
-                        break;
-                }
-                getSupportFragmentManager().beginTransaction().replace(R.id.content, currentFragment).setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE).commit();
-                return true;
+        bottomNavigationView.setOnNavigationItemSelectedListener(menuItem -> {
+            switch (menuItem.getItemId()) {
+                case R.id.summary:
+                    currentFragment = fragment[0];
+                    break;
+                case R.id.today:
+                    currentFragment = fragment[1];
+                    break;
+                default:
             }
+            getSupportFragmentManager().beginTransaction().replace(R.id.content, currentFragment).setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE).commit();
+            return true;
         });
     }
 
